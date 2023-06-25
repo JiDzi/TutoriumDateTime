54a03db8a45d3d475bc46cb2c4e3dd3581768265
Jiri <wi22b004@technikum-wien.at>
2023-06-25 10:18:39 +0200

    Initial commit

---
+++ src/main/java/com/example/TutoriumDateTime/TutoriumDateTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TutoriumDateTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoriumDateTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoriumDateTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TutoriumDateTime/TutoriumDateTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TutoriumDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoriumDateTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
